0e4c29f5815bd1452033f7b8c686d83a3cc5f9cc
Flurin <37112618+flurinh@users.noreply.github.com>
2020-01-18 22:30:26 +0100

    Delete run_eval.py

--- run_eval.py
+++
@@ -1,9 +0,0 @@
-import os
-
-
-RUNS = [14001, 14003, 14004, 14006, 15001, 15002, 15003, 15004, 15005, 15007, 15008, 15009, 15010]
-# RUNS = [15005]
-
-if __name__ == '__main__':
-    for run in RUNS:
-        os.system('bsub -W 1:00 -R "rusage[mem=10000]" python sandbox.py --run ' + str(run) + ' --mode 1')
